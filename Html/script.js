5b152bdc9d489c162fb6e3716a379582a920ef2b
Nicollas Delgado <nicollasrodriguesdelgado@gmail.com>
2025-06-02 20:45:03 -0300

    Update script.js

--- Html/script.js
+++ Html/script.js
@@ -1,1 +1,18 @@
+console.log("")
 
+const input = document.getElementById('task');
+const button = document.getElementById('add');
+const message = document.getElementById('message-error');
+
+button.addEventListener('click', () => {
+    if (input.value === '') {
+        input.classList.add('error');
+        message.classList.add('visible');
+        return
+    }
+
+    const li = document.createElement('li')
+    li.textContent = task.value
+    document.querySelector('ul').appendChild(li)
+
+});
